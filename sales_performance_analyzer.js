9325a228c41f533b8fb763c356b73ec10f088748
Rashi Ratan <rashiiratan@gmail.com>
2024-09-08 17:53:49 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -9,3 +9,21 @@ function calculateAverageSales(sales) {
         return AverageSales;
     //using if-else to ensure avoid the division by zero in case input is empty.
 }
+
+//Task 2:
+function determinePerformanceRating(AverageSales) {
+    let PerformanceRating;
+    if (AverageSales >= 10000) {
+       PerformanceRating = "Excellent";
+    } //using the highest number for the first condition
+    else if (AverageSales >= 7000 && AverageSales < 10000) {
+        PerformanceRating = "Good";
+    } //second highest for condition no. 2
+    else if (AverageSales >= 4000 && AverageSales < 7000) {
+        PerformanceRating = "Satisfactory";
+    } //third highest for condition no. 3
+    else {
+        PerformanceRating = "Needs Improvement";
+    } //for all other values
+    return PerformanceRating;
+}
