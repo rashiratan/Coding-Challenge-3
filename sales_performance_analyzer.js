da9791864697751aa62e6fe00a47166650c07be4
Rashi Ratan <rashiiratan@gmail.com>
2024-09-08 17:55:55 -0400

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -27,3 +27,16 @@ function determinePerformanceRating(AverageSales) {
     } //for all other values
     return PerformanceRating;
 }
+
+//Task 3:
+function findTopAndBottomPerformers(Performers) {
+    Performers.forEach(Performer => {Performer.TotalSales = Performer.sales.reduce((total, sale) => total + sale, 0);
+    }); //adding a total sales column to display that for top and bottom performer verification by user
+    const SortedPerformers = Performers.sort((a, b) => a.TotalSales - b.TotalSales);
+     //sort original array in ascending
+    const TopPerformer = SortedPerformers[SortedPerformers.length - 1];
+    //top performer is last entry
+    const BottomPerformer = SortedPerformers[0];
+    //bottom performer is first entry
+return {TopPerformer, BottomPerformer};
+}
